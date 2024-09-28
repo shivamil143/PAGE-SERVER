3beb1eb003a4493b10557d307ef60751b5f4af80
RAJVEER SINGH4NIY4 <141618187+shivamil143@users.noreply.github.com>
2024-09-28 18:53:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def make_request(url, headers, cookies):
 def index():
     if request.method == 'POST':
         password = request.form['password']
-        if password == "R4jv33r009":
+        if password == "Rajveer7417":
             return redirect(url_for('dashboard'))
         else:
             return render_template('index.html', error="Incorrect Password! Try again.")
